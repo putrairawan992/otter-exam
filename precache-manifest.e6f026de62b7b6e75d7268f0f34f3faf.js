713429726fdf724c04a54f72583ab7222cc6b920
putrairawan992 <putrairawan992@yahoo.com>
2020-05-26 13:27:24 +0700

    Updates

---
+++ precache-manifest.e6f026de62b7b6e75d7268f0f34f3faf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcd6c52b9ce2eb3bf1b964c345340243",
+    "url": "/otter-exam/index.html"
+  },
+  {
+    "revision": "744cb2512ce0e4e5c098",
+    "url": "/otter-exam/static/css/2.63b65d4e.chunk.css"
+  },
+  {
+    "revision": "0baae6700f46634b4a4a",
+    "url": "/otter-exam/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "744cb2512ce0e4e5c098",
+    "url": "/otter-exam/static/js/2.601b3ada.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/otter-exam/static/js/2.601b3ada.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0baae6700f46634b4a4a",
+    "url": "/otter-exam/static/js/main.9b1295d1.chunk.js"
+  },
+  {
+    "revision": "06f2c6be3c2e9a71dea5",
+    "url": "/otter-exam/static/js/runtime-main.8b391353.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/otter-exam/static/media/logo.5d5d9eef.svg"
+  }
+]);
